96637818ba66076beb3300d663f385f4829fc4ae
junjypark <73309648+junjypark@users.noreply.github.com>
2023-11-27 15:05:51 -0500

    Update SAN.R

--- R/SAN.R
+++ R/SAN.R
@@ -3,13 +3,13 @@
 #' 
 #' @param dat Numeric matrix with imaging features as rows,
 #'                and subjects as columns.
+#' @param batch Numeric or character vector specifying the batch/scanner 
+#'     variable needed for harmonization. 
+#' @param distMat Symmetric matrix indicating distance between imaging features.
 #' @param range  Radius for Stage 1; default 5.
 #' @param mod Optional model matrix for outcome of interest and 
 #'     other covariates besides batch/scanner.
 #' @param kernel SACFs for Stage 2; default \code{kernel="mixture"}; \code{kernel="exponential"} and \code{kernel="squared exponential"} are also supported.
-#' @param dis Symmetric matrix indicating distance between imaging features.
-#' @param batch Numeric or character vector specifying the batch/scanner 
-#'     variable needed for harmonization. 
 #' @param standardize Logical. TRUE by default; If TRUE, the output from Stage 1 is standardized.
 #' @param method The integration of harmonization method; default \code{method="None"};\code{method="RELIEF"} and \code{method="CovBat"} are also supported.
 #' @param stage1 Stage 1 model; default \code{stage1="combat"};\code{stage1="combat"} and \code{stage1="collapsing"} is also supported.
@@ -22,7 +22,9 @@
 #'     contains estimates and other parameters used during harmonization. 
 #' 
 
-SAN<-function(dat,range=5,mod=NULL, kernel='mixture',dis=NULL,batch=NULL,standardize=TRUE,method='None',stage1='combat',parallel=FALSE,ncores=2){
+SAN<-function(dat, batch=NULL, distMat=NULL, mod=NULL, 
+              range=5, kernel='mixture', standardize=TRUE, method='None',stage1='combat',
+              parallel=FALSE,ncores=2){
   
   batch.f=as.factor(batch)
   batch.id=unique(batch); n.batch=length(batch.id)
@@ -30,11 +32,11 @@ SAN<-function(dat,range=5,mod=NULL, kernel='mixture',dis=NULL,batch=NULL,standar
   count.batch=sapply(1:n.batch,function(m) sum(batch.f==batch.id[m]))
   
   if (stage1=='combat'){
-    stage1_result=local_combat(data.mat=dat,range=range,dis=dis,batches=batch,mod=mod,standardize = standardize,parallel=parallel,ncores=ncores)
+    stage1_result=local_combat(data.mat=dat,range=range,distMat=distMat,batches=batch,mod=mod,standardize = standardize,parallel=parallel,ncores=ncores)
   }else if (stage1=='collapsing'){
-    stage1_result=local_reg(data.mat=dat,range=range,dis=dis,batches=batch,mod=mod,standardize = standardize)
-    
+    stage1_result=local_reg(data.mat=dat,range=range,distMat=distMat,batches=batch,mod=mod,standardize = standardize)
   }
+  
   data.mat=stage1_result$data.mat
   
   p=nrow(data.mat)
@@ -49,65 +51,18 @@ SAN<-function(dat,range=5,mod=NULL, kernel='mixture',dis=NULL,batch=NULL,standar
   Xbeta=stage1_result$Xbeta
   
   epsilon=list()
-  for (i in 1:n.batch){
-    epsilon[[i]]=data.mat[,batch==batch.id[i]]
-  }
+  for (i in 1:n.batch){ epsilon[[i]]=data.mat[,batch==batch.id[i]] }
   
-  if (kernel=="exponential"){corMat.base=exp(-dis)
-  
-  phi.hat=optimize(CovRegOptim.1,interval=c(10^-5, 10),epsilon=epsilon, corMat_base=corMat.base)$'minimum'
-  varcomps=ObtainVarComps.1(phi.hat, epsilon=epsilon, corMat.base)
-  sigma=varcomps$sigma
-  tau=varcomps$tau
-  harmonization_result=harmonization_step(kernel='exponential',sigma,tau,phi.hat,data.mat,count.batch,dis,batch.f,batch.id,method=method,parallel=parallel,ncores=ncores)
-  delta_h=harmonization_result$delta_h
-  gamma_h=harmonization_result$gamma_h
-  delta=harmonization_result$delta
-  gamma=harmonization_result$gamma
-  sigma_h=harmonization_result$sigma_h
-  tau_h=harmonization_result$tau_h
-  
-  if (standardize){
-    epsilon_h=(delta_h+gamma_h)*sigma.mat.h
-  }else{
-    epsilon_h=delta_h+gamma_h
-  }
-  
-  data_h=alpha+Xbeta+epsilon_h
-  }
-  else if (kernel=="squared exponential"){corMat.base=exp(-dis^2)
-  
-  phi.hat=optimize(CovRegOptim.1,interval=c(10^-5, 10),epsilon=epsilon, corMat_base=corMat.base)$'minimum'
-  varcomps=ObtainVarComps.1(phi=phi.hat, epsilon=epsilon, corMat_base=corMat.base)
-  sigma=varcomps$sigma
-  tau=varcomps$tau
-  
-  harmonization_result=harmonization_step(kernel='squared exponential',sigma,tau,phi.hat,data.mat,count.batch,dis,batch.f,batch.id,method=method,parallel=parallel,ncores=ncores)
-  delta_h=harmonization_result$delta_h
-  gamma_h=harmonization_result$gamma_h
-  delta=harmonization_result$delta
-  gamma=harmonization_result$gamma
-  sigma_h=harmonization_result$sigma_h
-  tau_h=harmonization_result$tau_h
-  if (standardize){
-    epsilon_h=(delta_h+gamma_h)*sigma.mat.h
-  }else{
-    epsilon_h=delta_h+gamma_h
-  }
-  
-  data_h=alpha+Xbeta+epsilon_h
-  
-  }
-  else if (kernel=='mixture'){
-    corMat.base1=exp(-dis)
-    corMat.base2=exp(-dis^2)
-    
-    phi.hat=optim(c(0.01, 0.01), CovRegOptim.2,epsilon=epsilon, corMat_base1=corMat.base1,
-                  corMat_base2=corMat.base2)$par
-    varcomps=ObtainVarComps.2(phi.hat, epsilon=epsilon,  corMat.base1, corMat.base2)
-    sigma=matrix(varcomps$sigma,nrow=n.batch)
+  if (kernel=="exponential"){
+    corMat.base=exp(-distMat)
+    phi.hat=optimize(CovRegOptim.1,interval=c(10^-5, 10),epsilon=epsilon, corMat_base=corMat.base)$'minimum'
+    varcomps=ObtainVarComps.1(phi.hat, epsilon=epsilon, corMat.base)
+    sigma=varcomps$sigma
     tau=varcomps$tau
-    harmonization_result=harmonization_step(kernel='mixture',sigma,tau,phi.hat,data.mat,count.batch,dis,batch.f,batch.id,method=method,parallel=parallel,ncores=ncores)
+    harmonization_result=harmonization_step(
+      kernel='exponential',sigma,tau,phi.hat,
+      data.mat,count.batch,distMat,batch.f,batch.id,
+      method=method,parallel=parallel,ncores=ncores)
     
     delta_h=harmonization_result$delta_h
     gamma_h=harmonization_result$gamma_h
@@ -116,18 +71,63 @@ SAN<-function(dat,range=5,mod=NULL, kernel='mixture',dis=NULL,batch=NULL,standar
     sigma_h=harmonization_result$sigma_h
     tau_h=harmonization_result$tau_h
     
-    if (standardize){
-      epsilon_h=(delta_h+Reduce('+',gamma_h))*sigma.mat.h
-    }else{
-      epsilon_h=delta_h+Reduce('+',gamma_h)
-    }
+    if (standardize){ epsilon_h=(delta_h+gamma_h)*sigma.mat.h }
+    else{ epsilon_h=delta_h+gamma_h }
     
     data_h=alpha+Xbeta+epsilon_h
+  }
+  else if (kernel=="squared exponential"){
+    corMat.base=exp(-distMat^2)
+  
+    phi.hat=optimize(CovRegOptim.1,interval=c(10^-5, 10),epsilon=epsilon, corMat_base=corMat.base)$'minimum'
+    varcomps=ObtainVarComps.1(phi=phi.hat, epsilon=epsilon, corMat_base=corMat.base)
+    sigma=varcomps$sigma
+    tau=varcomps$tau
     
+    harmonization_result=harmonization_step(
+      kernel='squared exponential',sigma,tau,phi.hat,
+      data.mat,count.batch,distMat,batch.f,batch.id,
+      method=method,parallel=parallel,ncores=ncores)
+    delta_h=harmonization_result$delta_h
+    gamma_h=harmonization_result$gamma_h
+    delta=harmonization_result$delta
+    gamma=harmonization_result$gamma
+    sigma_h=harmonization_result$sigma_h
+    tau_h=harmonization_result$tau_h
+    
+    if (standardize){ epsilon_h=(delta_h+gamma_h)*sigma.mat.h }
+    else{ epsilon_h=delta_h+gamma_h }
+    
+    data_h=alpha+Xbeta+epsilon_h
+  }
+  else if (kernel=='mixture'){
+    corMat.base1=exp(-distMat)
+    corMat.base2=exp(-distMat^2)
+    
+    phi.hat=optim(c(0.01, 0.01), CovRegOptim.2,epsilon=epsilon, corMat_base1=corMat.base1,
+                  corMat_base2=corMat.base2)$par
+    varcomps=ObtainVarComps.2(phi.hat, epsilon=epsilon,  corMat.base1, corMat.base2)
+    sigma=matrix(varcomps$sigma,nrow=n.batch)
+    tau=varcomps$tau
+
+    harmonization_result=harmonization_step(
+      kernel='mixture',sigma,tau,phi.hat,
+      data.mat,count.batch,distMat,batch.f,batch.id,
+      method=method,parallel=parallel,ncores=ncores)
+    
+    delta_h=harmonization_result$delta_h
+    gamma_h=harmonization_result$gamma_h
+    delta=harmonization_result$delta
+    gamma=harmonization_result$gamma
+    sigma_h=harmonization_result$sigma_h
+    tau_h=harmonization_result$tau_h
+    
+    if (standardize){ epsilon_h=(delta_h+Reduce('+',gamma_h))*sigma.mat.h }
+    else{ epsilon_h=delta_h+Reduce('+',gamma_h) }
+    
+    data_h=alpha+Xbeta+epsilon_h
   }
   
-  
   estimates=list(alpha=alpha,Xbeta=Xbeta,sigma.mat.h=sigma.mat.h,phi.hat=phi.hat,sigma=sigma,tau=tau,sigma_h=sigma_h,tau_h=tau_h,delta=delta,gamma=gamma,delta_h=delta_h,gamma_h=gamma_h)
-  
   return(list(data_h=data_h,data_res=data.mat,epsilon_h=epsilon_h,estimates=estimates))
 }
